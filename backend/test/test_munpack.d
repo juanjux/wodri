0eff8e648e1633601eabc1e3d2dfbb029c317e60
Juanjo Alvarez <juanjux@almarsoft.com>
2014-05-28 12:27:57 +0200

    test_munpack for testing the reliability of munpack extracting parts of a multipart email

--- backend/test/test_munpack.d
+++ backend/test/test_munpack.d
@@ -5,8 +5,28 @@ import std.file;
 import std.path;
 import std.conv;
 import std.string;
+import std.process;
+
+/* Test reliability of munpack */
 
 void main() {
+    
+    void run_munpack(string mailpath) 
+    {
+        // Create a dir for the parts
+        auto orig_dir = getcwd();
+        auto parts_dir = mailpath ~ "_dir";
+        mkdir(parts_dir);
+        chdir(parts_dir);
+
+        auto munpack_cmd = "munpack -t ../" ~ baseName(mailpath);
+        //writeln(getcwd());
+        //writeln(munpack_cmd);
+        auto munpack = executeShell(munpack_cmd);
+        if (munpack.status != 0) throw new Exception("munpack failed for: " ~ mailpath);
+        chdir(orig_dir);
+    }
+
     auto mbox_file = "emails/with_attachments/conadjuntos.mbox";
     auto single_mails_dir = "emails/with_attachments/single_mails";
 
@@ -25,11 +45,15 @@ void main() {
     while (!mboxf.eof()) {
         line = chomp(mboxf.readln());
         if (line.length > 6 && line[0..5] == "From ") {
-            // New email
+            if (email_file.isOpen) {
+                email_file.flush();
+                email_file.close();
+                run_munpack(email_file.name);
+            }
+
             email_file = File(buildPath(single_mails_dir, to!string(++mailindex)), "w");
             writeln(mailindex);
         }
-
         email_file.write(line ~ "\r\n");
     }
 
