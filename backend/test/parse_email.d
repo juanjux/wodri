71af2032642ae40a3dfa5275c05eb874065c9e85
Juanjo Alvarez <juanjux@almarsoft.com>
2014-06-04 17:42:14 +0200

    MIME body tests and test data generation

--- backend/test/parse_email.d
+++ backend/test/parse_email.d
@@ -1,7 +1,7 @@
 #!/usr/bin/env rdmd 
 
 import std.stdio;
-import std.file: dirEntries, DirEntry, SpanMode;
+import std.file: dirEntries, DirEntry, SpanMode, isDir, exists, mkdir;
 import std.path;
 import std.conv;
 import std.algorithm;
@@ -23,7 +23,7 @@ import lib.dictionarylist; import lib.characterencodings;
 
 class MIMEPart
 {
-    MIMEPart* parent = null;
+    MIMEPart parent = null;
     MIMEPart[] subparts;
     ContentData ctype;
     ContentData disposition;
@@ -208,15 +208,12 @@ class ProtoEmail
         bool finished = false;
         int globalIndex = startIndex;
 
-        writeln("XXX parseParts");
-
         while (!finished && (globalIndex <= lines.length))
         {
             endIndex = -1;
             // Find the next boundary
             foreach(int j, string bline; lines[startIndex..$])
             {
-                writeln("XXX MIERDA");
                 if (strip(bline) == boundaryPart)
                 {
                     endIndex = startIndex+j;
@@ -229,17 +226,13 @@ class ProtoEmail
                     break;
                 }
             }
-            writeln("XXX 0");
             if (endIndex == -1) 
                 return; // correct?
 
-            writeln("XXX 2", globalIndex);
             MIMEPart thisPart = new MIMEPart();
-            writeln("XXX 3", globalIndex);
             int contentStart = parsePartHeaders(thisPart, lines[startIndex..endIndex]);
-
-            writeln("XXX 4", globalIndex);
             parent.subparts ~= thisPart;
+            thisPart.parent = parent;
 
             if (thisPart.ctype.name.length > 9 && thisPart.ctype.name[0..9] == "multipart")
                 parseParts(lines[startIndex..endIndex], thisPart.ctype.fields["boundary"], thisPart);
@@ -258,7 +251,7 @@ class ProtoEmail
             // XXX thisPart.disposition.name == "attachment" || "inline":
             // 1. sacar el contenido, decodificarlo
             // 2. guardar en emails/attachments con un nombre unico
-            // 3. poner como contenido <<ruta>>
+            // 3. poner como contenido <<ruta>> (o nada)
 
             startIndex = endIndex+1;
             ++globalIndex;
@@ -371,7 +364,7 @@ class ProtoEmail
         if ("Content-Transfer-Encoding" in this.headers)
             part.content_transfer_encoding = strip(removechars(this.headers["Content-Transfer-Encoding"], "\""));
 
-        if ("charset" !in part.ctype.fields)
+        if (!part.ctype.name.startsWith("multipart") && "charset" !in part.ctype.fields)
             part.ctype.fields["charset"] = "latin1";
     }
 
@@ -410,14 +403,86 @@ class ProtoEmail
 }
 
 
+
+// ############### TESTING CODE ###########################
+
+
+// ponerle version unittest, debug
+DirEntry[] getSortedEmailFilesList(string mailsDir)
+{
+    DirEntry[] emailFiles;
+    foreach(DirEntry e; dirEntries(mailsDir, SpanMode.shallow))
+        if (!e.isDir) emailFiles ~= e;
+
+    bool intFileComp(DirEntry x, DirEntry y) 
+    {
+        return to!int(baseName(x.name)) < to!int(baseName(y.name));
+    }
+    sort!(intFileComp)(emailFiles);
+
+    return emailFiles;
+}
+
+// version debug, unittest
+void createPartInfoText(MIMEPart part, ref Appender!string ap, int level)
+{
+    string parentStr;
+
+    ap.put(format("==#== PART ==#==\n"));
+    if (part.parent !is null) 
+        ap.put(format("Son of: %s\n", part.parent.ctype.name));
+    else 
+        ap.put("Root part\n");
+        
+    ap.put(format("Level: %d\n", level));
+
+    ap.put(format("Content-Type: %s\n", part.ctype.name));
+    ap.put("\tfields: \n");
+    if ("charset" in part.ctype.fields)        
+        ap.put(format("\t\tcharset: %s\n", part.ctype.fields["charset"]));
+    if ("boundary" in part.ctype.fields)       
+        ap.put(format("\t\tboundary: %s\n", part.ctype.fields["boundary"]));
+    if (part.disposition.name.length)
+    {
+        ap.put(format("Content-Disposition: %s\n", part.disposition.name));
+        if ("filename" in part.disposition.fields)
+            ap.put(format("\t\tfilename: %s\n", part.disposition.fields["filename"]));
+    }
+    if (part.content_transfer_encoding.length) 
+        ap.put(format("Content-Transfer-Encoding: %s\n", part.content_transfer_encoding));
+
+    // attachments are compared with an md5 on the files, not here
+    if (part.textContent.length > 0 && !among(part.disposition.name, "attachment", "inline"))
+    {
+        ap.put(format("Content Length: %d\n", part.textContent.length));
+        ap.put("##=## CONTENT ##=##\n");
+        ap.put(part.textContent);
+        ap.put("##=## ENDCONTENT ##=##\n");
+    }
+
+    ++level;
+    foreach(MIMEPart subpart; part.subparts)
+        createPartInfoText(subpart, ap, level);
+}
+
+
 void main()
 {
-    //enum State {Disabled, Normal, GenerateTestData};
-    //State state = State.Normal;
-    //version(unittest) state = Disabled;
-    uint status = 0;
-    if (status == 0)
+
+    enum State {Disabled, Normal, GenerateTestData}
+
+    version(unittest) 
+        State state = State.Disabled;
+    else
+        State state = State.GenerateTestData;
+
+    string mailsDir = "emails/single_emails";
+
+    switch(state)
     {
+        case State.Normal:
+            // Specific tests
+
             // 22668 => multipart base64
             // 1973  => text/plain UTF-8 quoted-printable
             // 10000 => text/plain UTF-8 7bit
@@ -425,10 +490,46 @@ void main()
             // 50000 => multipart/alternative, text/plain sin encoding 7 bit y fuera de parte, text/html ISO8859-1 base64
             // 60000 => multipart/alternative Windows-1252 quoted-printable
             // 80000 => multipart/alternative ISO8859-1 quoted-printable
-            writeln("ENTRANDO XXX");
             auto filenumber = 22668;
-            auto email_file = File(format("emails/single_emails/%d", filenumber), "r"); // text/plain UTF-8 quoted-printable
+            auto email_file = File(format("%s/%d", mailsDir, filenumber), "r"); // text/plain UTF-8 quoted-printable
             auto email = new ProtoEmail(email_file);
+        break;
+
+        case State.GenerateTestData:
+            // For every mail in maildir, parse, create a mailname_test dir, and create a testinfo file inside 
+            // with a description of every mime part (ctype, charset, transfer-encoding, disposition, length, etc) 
+            // and their contents. This will be used in the unittest for comparing the email parsing output with
+            // these. Obviously, it's very important to regenerate these files only with Good and Tested versions :)
+            auto sortedFiles = getSortedEmailFilesList(mailsDir);
+            foreach(DirEntry e; sortedFiles)
+            {
+                // parsear email e.name
+                // sacar info de partes desde la principal
+                // - ctype, charset, content-disposition, length, subparts, parent, transfer-encoding, contenido
+                // guardar en fichero
+                writeln("Generating testfile for ", e.name);
+                if (e.name.isDir) 
+                    continue;
+
+                auto testDir = format("%s_t", e.name);
+                if (!testDir.exists) 
+                    mkdir(testDir);
+
+                auto attachDir = buildPath(testDir, "attachments");
+                if (!attachDir.exists) 
+                    mkdir(attachDir);
+
+                auto email = new ProtoEmail(File(e.name));
+                auto ap = appender!string;
+                createPartInfoText(email.rootPart, ap, 0);
+                auto testFile = buildPath(testDir, "mime_info.txt");
+                auto f = File(testFile, "w");
+                f.write(ap.data);
+                f.close();
+            }
+        break;
+
+        default:
     }
 }
 
@@ -438,26 +539,15 @@ unittest
     /* For every mail in the testing-mails repo, parse the email, print the headers
      * and compare line by line with the (decoded) original */
     writeln("Starting unittest");
-    string repodir = "emails/single_emails/";
+    string mailsDir = "emails/single_emails/";
 
     // Dont these these mails 
     int[string] brokenMails = ["53290":0, "64773":0, "87900":0, "91208":0, "91210":0];
 
 
-    DirEntry[] emailFiles;
-    foreach(DirEntry e; dirEntries(repodir, SpanMode.shallow))
-        emailFiles ~= e;
-
-    bool intFileComp(DirEntry x, DirEntry y) 
+    foreach (DirEntry e; getSortedEmailFilesList(mailsDir))
     {
-        return to!int(baseName(x.name)) < to!int(baseName(y.name));
-    }
-    sort!(intFileComp)(emailFiles);
-
-    foreach (DirEntry e; emailFiles)
-    {
-        //if (indexOf(e.name, "7038") == -1) continue;
-        writeln(e.name);
+        writeln(e.name, "...");
         if (baseName(e.name) in brokenMails) continue;
 
         auto email = new ProtoEmail(File(e.name));
@@ -487,6 +577,34 @@ unittest
                 //break;
             }
             ++idx;
+        }
+        writeln("\t\t...headers ok!");
+
+        auto testFilePath = buildPath(format("%s_t", e.name), "mime_info.txt");
+        auto f = File(testFilePath, "r");
+        auto ap1 = appender!string();
+        auto ap2 = appender!string();
+
+        while(!f.eof)
+        {
+            ap1.put(f.readln());
+        }
+        createPartInfoText(email.rootPart, ap2, 0);
+
+        if (ap1.data == ap2.data)
+            writeln("\t\t...MIME parts ok!");
+        else
+        {
+            writeln("Body parts different");
+            writeln("Parsed email: ");
+            writeln("----------------------------------------------------");
+            write(ap1.data);
+            writeln("----------------------------------------------------");
+            writeln("Text from testfile: ");
+            writeln("----------------------------------------------------");
+            write(ap2.data);
+            writeln("----------------------------------------------------");
+            assert(0);
         }
     }
 }
